f8c036e9a398f4581f71117f5da8adbd56695e27
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2023-11-18 05:40:13 +0530

    do it

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-    production: true,
-    apiKey: 'a18b63e62753920054f7cb148922bb01', // Replace with your actual OpenWeatherMap API key
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.ts
-// Development environment
-export const environment = {
-    production: false,
-    apiKey: 'a18b63e62753920054f7cb148922bb01', // Replace with your actual OpenWeatherMap API key
-  };
-  
